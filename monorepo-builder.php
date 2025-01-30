f9b6a913d42d59cf322573c92b8e46da369e80ce
Philip Gichuhi <pgichuhi@microsoft.com>
2025-01-30 17:59:36 +0300

    feat: use central config for unit tests and static analysis

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ValueObject\Option;
 use Symplify\MonorepoBuilder\Config\MBConfig;
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -7,6 +9,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 return static function (MBConfig $mbConfig): void {
     // where are the packages located?
     $mbConfig->packageDirectories([
-        __DIR__ . '/packages',
+        __DIR__ . '/packages/',
+    ]);
+    $mbConfig->dataToRemove([
+        ComposerJsonSection::MINIMUM_STABILITY => Option::REMOVE_COMPLETELY,
+        ComposerJsonSection::REPOSITORIES => [
+            Option::REMOVE_COMPLETELY,
+        ],
+        ComposerJsonSection::REPLACE => [
+            Option::REMOVE_COMPLETELY,
+        ]
     ]);
 };
