977d6571a7c708abbdd20ca8655f60cc9056b1ca
Vincent Biret <vibiret@microsoft.com>
2026-02-24 13:53:22 -0500

    fix!: remove support for php 7.2 through 8.2 to address security issues
    Release-As: 2.0.0
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/abstractions/src/Serialization/AdditionalDataHolder.php
+++ packages/abstractions/src/Serialization/AdditionalDataHolder.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Microsoft\Kiota\Abstractions\Serialization;
-
+/**
+ * Interface for models that hold additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
+ */
 interface AdditionalDataHolder {
     /**
      * Gets the additional data for this object that did not belong to the properties.
